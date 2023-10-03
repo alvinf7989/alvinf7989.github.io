c0fb5f16a969bda2339e4b8b05d6adfe06dd4740
alvinf7989 <146861846+alvinf7989@users.noreply.github.com>
2023-10-03 12:02:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Alvin Frias"; // update the part BETWEEN the "quotes"
+let userName = "alvinf7989"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
